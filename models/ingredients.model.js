a3168b2fe40be08151b042ff504b27416c0b72dc
Ken-Warner <ken.c.warner@gmail.com>
2023-11-05 14:06:00 -0500

    Create ingredients API endpoint created and tested for singular raw ingredients.

--- models/ingredients.model.js
+++ models/ingredients.model.js
@@ -1,26 +1,81 @@
 const { query } = require('./pool');
 
-async function createIngredient() {
+async function createIngredientRaw(userid, ingredient) {
+  //For batch input
+  // let createIngredientQuery = {
+  //   text: `INSERT INTO ingredients
+  //             (user_id, name, description, calories, protein, carbohydrates, fats)
+  //             VALUES ${ingredients.map(ingredient => {
+  //     return `($1,'${ingredient.name}','${ingredient.description || ''}',${ingredient.calories || 0},${ingredient.protein || 0},${ingredient.carbohydrates || 0},${ingredient.fats || 0})`;
+  //   }).join(',')}
+  //             RETURNING *;`,
+  //   params: [
+  //     userid
+  //   ],
+  // };
+  let createIngredientQuery = {
+    text: `INSERT INTO ingredients
+            (user_id, name, description, calories, protein, carbohydrates, fats)
+            VALUES ($1, $2, $3, $4, $5, $6, $7)
+            RETURNING *;`,
+    params: [
+      userid,
+      ingredient.name,
+      ingredient.description || '',
+      ingredient.calories || 0,
+      ingredient.protein || 0,
+      ingredient.carbohydrates || 0,
+      ingredient.fats || 0,
+    ],
+  };
 
+  try {
+    const result = await query(createIngredientQuery);
+
+    if (result.rowCount == 1)
+      return result.rows[0];
+  } catch (e) {
+    throw new Error('Could not create new ingredient:' + e.message);
+  }
+}
+
+async function createIngredientFromComponents(userid, components) {
+  //select all components with the provided ids
+  let getComponentsQuery = {
+    text: `SELECT id, calories, protein, carbohydrates, fats
+            FROM ingredients
+            WHERE id = ANY($1::int[])
+            AND user_id = $2;`,
+    params: [
+      components.map(component => component.id),
+      userid,
+    ],
+  };
+
+  const componentsForNewIngredient = await query(getComponentsQuery);
+  console.log(componentsForNewIngredient);
+  //using the portion size, tally up all the macros into a new ingredient
+  //use create ingredient raw to create the new ingredient??
+    //else just do a new query
 }
 
 async function deleteIngredientById(userId, ingredientId) {
-    let deleteIngredientQuery = {
-        text: `DELETE FROM ingredients
+  let deleteIngredientQuery = {
+    text: `DELETE FROM ingredients
                 WHERE user_id = $1
                 AND id = $2;`,
-        params: [
-            userId,
-            ingredientId,
-        ],
-    };
+    params: [
+      userId,
+      ingredientId,
+    ],
+  };
 
-    const result = await query(deleteIngredientQuery);
-    
-    if (result.rowCount == 1)
-      return;
-    else
-      throw new Error('No Ingredient Found with that ID');
+  const result = await query(deleteIngredientQuery);
+
+  if (result.rowCount == 1)
+    return;
+  else
+    throw new Error('No Ingredient Found with that ID');
 }
 
 async function getIngredientsByUserId(userId) {
@@ -42,7 +97,8 @@ async function getIngredientsByUserId(userId) {
 }
 
 module.exports = {
-  createIngredient,
+  createIngredientRaw,
+  createIngredientFromComponents,
   deleteIngredientById,
   getIngredientsByUserId,
 };
--- routes/ingredients/ingredients.controller.js
+++ routes/ingredients/ingredients.controller.js
@@ -1,9 +1,10 @@
 const path = require('path');
 
 const {
-    createIngredient,
-    deleteIngredientById,
-    getIngredientsByUserId,
+  createIngredientRaw,
+  createIngredientFromComponents,
+  deleteIngredientById,
+  getIngredientsByUserId,
 } = require('../../models/ingredients.model');
 
 async function createNewIngredient(req, res) {
@@ -12,15 +13,38 @@ async function createNewIngredient(req, res) {
     return;
   }
 
-  //server side validations
-  //determine which type of ingredient to create
-    //one of other ingredients
-    //or a raw ingredient
-
   try {
+    if (req.body.components && req.body.components.length > 0) {
+      //ingredient is composed of other existing ingredients
+      //all macro information is ignored and instead is tallied from the
+      //existing ingredients
+      if (req.body.components.some(component => component.ingredientId == 0) || req.body.components.some(component => component.portionSize == 0)) {
+        res.status(400).send(JSON.stringify({ error: "Not all component IDs or portion sizes are provided" }));
+        return;
+      }
 
+      const newIngredient = await createIngredientFromComponents(req.session.userid, req.body.components);
+
+      res.status(201).send(JSON.stringify(newIngredient));
+
+    } else if (req.body.ingredient && req.body.ingredient.name != '') {
+
+      //make sure all names are populated (FOR BATCH VALIDATION)
+      // if (req.body.ingredients.some(ingredient => ingredient.name == '')) {
+      //   res.status(400).send(JSON.stringify({ error: "An ingredient name must be provided." }));
+      //   return;
+      // }
+
+      const newIngredient = await createIngredientRaw(req.session.userid, req.body.ingredient);
+
+      res.status(201).send(JSON.stringify(newIngredient));
+      return;
+    } else {
+      res.status(400).send(JSON.stringify({ error: "Invalid Input" }));
+    }
   } catch (e) {
-    res.status(400).send(e.message);
+    res.status(500).send(JSON.stringify({ error: `An error occured: ${e.message}` }));
+    return;
   }
 }
 
@@ -32,7 +56,7 @@ async function deleteIngredient(req, res) {
 
   try {
     const result = await deleteIngredientById(req.session.userid,
-                                              req.params.ingredientId);
+      req.params.ingredientId);
 
     res.status(200).send();
   } catch (e) {
