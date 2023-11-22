bd3f5f7b1fcf40f84c1919c02166a2ba9e3c8255
Ken-Warner <ken.c.warner@gmail.com>
2023-11-21 19:50:59 -0500

    Weigh In API created the POST method, tested with Postman.

--- models/weighIn.model.js
+++ models/weighIn.model.js
@@ -1,7 +1,31 @@
-const { query } = require('./pool');
+const {
+  query,
+  DEFAULT,
+  buildInsert,
+ } = require('./pool');
 
 async function insertWeighInData(userid, weighInData) {
 
+  const fields = [ 'user_id', 'weight', 'date' ];
+  const values = [ userid, weighInData.weight, weighInData.date || DEFAULT ];
+
+  [
+    queryFields,
+    queryValues,
+    queryParams
+  ] = buildInsert(fields, values);
+
+  let insertWeighInDataQuery = {
+    text: `INSERT INTO user_weights ${queryFields}
+            VALUES ${queryValues}
+            RETURNING *;`,
+    params: queryParams,
+  };
+
+  const result = await query(insertWeighInDataQuery);
+
+  if (result.rowCount != 1)
+    throw new Error('Unable to insert new weight');
 }
 
 async function selectWeighInDataForDateRange(userid, fromDate, toDate) {
--- routes/weighIn/weighIn.controller.js
+++ routes/weighIn/weighIn.controller.js
@@ -23,13 +23,34 @@ async function postWeighInData(req, res) {
     return;
   }
 
-  //validate weight data
-  //validate optional input date
+  let weighInData = {};
 
-  //call model to post new data
+  if (!req.body.weight || req.body.weight <= 0) {
+    res.status(400).send(JSON.stringify({ error: `Please provide a valid weight.` }));
+    return;
+  }
 
-  res.status(200).send();
-  return;
+  if (req.body.date) {
+    const dateRegex = /\d{4}-\d{2}-\d{2}/;
+
+    if (!dateRegex.test(req.body.date)) {
+      res.status(400).send(JSON.stringify({ error: `Please provide a date in the format YYYY-MM-DD` }));
+      return;
+    }
+  }
+
+  weighInData = {
+    weight: req.body.weight,
+    date: req.body.date,
+  };
+
+  try {
+    await insertWeighInData(req.session.userid, weighInData);
+
+    res.status(200).send();
+  } catch (e) {
+    res.status(500).send(JSON.stringify({ error: `An unexpected error occurred: ${e.message}` }));
+  }
 }
 
 module.exports = {
