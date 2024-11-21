30b66387e0da2a625abce0aacfc90bbeddaee3c2
Ken Warner <50887189+Ken-Warner@users.noreply.github.com>
2024-11-20 20:20:19 -0500

    Converting to React frontend.

--- app.js
+++ app.js
@@ -1,42 +1,37 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const sessions = require('express-session');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const sessions = require("express-session");
+const cookieParser = require("cookie-parser");
 
-const apiRouter = require('./routes/api.router');
-const uiRouter = require('./routes/tmpui/ui.router');
+const apiRouter = require("./routes/api.router");
+const uiRouter = require("./routes/tmpui/ui.router");
 
 const app = express();
-
-//set up handlebars
-app.set('view engine', 'hbs');
-app.emit('views', path.join(__dirname, 'views'));
 
 //expect json
 app.use(express.json());
 
-//setup static domains to provide JS and CSS for views
-app.use('/css', express.static(path.join(__dirname, 'public', 'css')));
-app.use('/js', express.static(path.join(__dirname, 'public', 'js')));
+app.use(express.static(path.join(__dirname, "public")));
 
 //setup session variables
 app.use(express.urlencoded({ extended: true }));
-app.use(sessions({
-    secret: process.env.SESSION_SECRET || 'somethingsecret',
+app.use(
+  sessions({
+    secret: process.env.SESSION_SECRET || "somethingsecret",
     saveUninitialized: true,
     cookie: {
-        maxAge: 24 * 60 * 60 * 1000 //1 day
+      maxAge: 24 * 60 * 60 * 1000, //1 day
     },
     resave: false,
-}));
+  })
+);
 app.use(cookieParser());
 
-app.use('/api', apiRouter);
-app.use('', uiRouter);
+app.use("/api", apiRouter);
 
-app.all('/*', (req, res) => {
-    res.status(404).send(JSON.stringify({ error: `The resource you were looking for could not be found.` }));
+app.all("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 module.exports = app;
